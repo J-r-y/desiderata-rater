f030b7fd74269bc880a8069843eba3c18dcd0708
Jerry <trikis2007@gmail.com>
2025-06-17 13:57:33 +0200

    feat: show/hide points on click

--- src/components/game/lobby/lobby.tsx
+++ src/components/game/lobby/lobby.tsx
@@ -19,6 +19,7 @@ export function Lobby(props: { player: PlayerType, lobby: LobbyType }) {
     const [lobby, setLobby] = useState<LobbyType>(props.lobby)
     const [players, setPlayers] = useState<PlayerType[]>(lobby.players.filter(p => p.id !== player.id))
     const [cards, setCards] = useState<string[]>([])
+    const [showPoints, setShowPoints] = useState<boolean>(true)
     const {socket} = useSocket()
 
     const selectCard = (card: string) => {
@@ -52,19 +53,21 @@ export function Lobby(props: { player: PlayerType, lobby: LobbyType }) {
 
     return (
         <div className={"flex flex-col items-center justify-center"}>
-            <Card className={"absolute top-5 left-5 min-w-24 gap-2"}>
+            <Card className={"absolute top-5 left-5 min-w-24 gap-2"} onClick={() => setShowPoints(!showPoints)}>
                 <CardHeader>
                     <CardTitle>Punkte</CardTitle>
                 </CardHeader>
-                <CardDescription className={"pl-6 pr-6 mt-0 pt-0"}>
-                    Lobby Code: {lobby.code}
-                </CardDescription>
-                <CardContent>
-                    <ul>
-                        {lobby.players.sort((a, b) => b.points - a.points).map((player, i) =>
-                            <li key={i}>{player.name}: {player.points}</li>)}
-                    </ul>
-                </CardContent>
+                {showPoints ? <>
+                    <CardDescription className={"pl-6 pr-6 mt-0 pt-0"}>
+                        Lobby Code: {lobby.code}
+                    </CardDescription>
+                    <CardContent>
+                        <ul>
+                            {lobby.players.sort((a, b) => b.points - a.points).map((player, i) =>
+                                <li key={i}>{player.name}: {player.points}</li>)}
+                        </ul>
+                    </CardContent>
+                </> : ""}
             </Card>
             {{
                 select: <CardSelect cards={cards} callback={selectCard}/>,
